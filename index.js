f6bc9d4f6af6f10e20f3e44ce607bf382bc3f396
Roshan Prasad <roshanprasad062@gmail.com>
2023-03-03 20:11:10 +0530

    latest commit

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import express from "express";
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
-import connectDB from "../mongodb/connect.js";
-import postRoutes from '../routes/postRoutes.js';
-import dalleRoutes from '../routes/dalleRoutes.js';
+import connectDB from "./server/mongodb/connect.js";
+import postRoutes from './server/routes/postRoutes.js';
+import dalleRoutes from './server/routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
